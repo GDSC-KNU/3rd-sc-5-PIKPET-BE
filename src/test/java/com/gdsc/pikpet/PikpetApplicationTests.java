ee7c749bcc14398cc58ed350dec21704eebf2439
jaehyeon <fanta4715@naver.com>
2024-01-27 10:46:34 +0900

    #1 init

---
+++ src/test/java/com/gdsc/pikpet/PikpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.pikpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

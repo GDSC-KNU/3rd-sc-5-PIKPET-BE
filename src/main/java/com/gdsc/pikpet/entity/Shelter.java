635bd4636ed2d9a5d2ca146ac47927b091eb642f
yooonwodyd <dbswodyd00@naver.com>
2024-01-30 07:59:40 +0900

    #6 Shelter class 구현

---
+++ src/main/java/com/gdsc/pikpet/entity/Shelter.java
@@ -0,0 +1,20 @@
+package com.gdsc.pikpet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+public class Shelter {
+    @Column(nullable = false)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int shelterId;
+
+    @Setter private double latitude;
+    @Setter private double longitude;
+    @Setter private String branchName;
+    @Setter private String contact;
+    // Todo: mangerID의 경우 리팩토링이 필요할 수 있음. OneToOne으로 연결시 문제 가능성. ManyToOne으로 변경 고려
+    @Setter private int managerId;
+}

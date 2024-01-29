283604f22723df873ccc80bee675e7d1e1b7e9ce
yooonwodyd <dbswodyd00@naver.com>
2024-01-30 07:59:54 +0900

    #6 UserAccount class 구현

---
+++ src/main/java/com/gdsc/pikpet/entity/UserAccount.java
@@ -0,0 +1,41 @@
+package com.gdsc.pikpet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+public class UserAccount {
+
+    @Id @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Setter private String email;
+    @Setter @Column(length = 100, nullable = false) private String password;
+    @Setter private String phoneNumber;
+    @Setter private Gender gender;
+    @Setter private int age;
+    @Setter private String address;
+    @Setter private String job;
+    @Setter @Enumerated(EnumType.STRING) private UserRole userRole;
+
+    protected UserAccount(){};
+
+    private UserAccount(String email,String password,String phoneNumber,Gender gender,int age,String address,String job, UserRole userRole){
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.age = age;
+        this.address = address;
+        this.job = job;
+        this.userRole = userRole;
+    }
+
+    public static UserAccount of(String email,String password){
+        return new UserAccount(email,password,null,null,0,null,null,UserRole.USER);
+    }
+
+}

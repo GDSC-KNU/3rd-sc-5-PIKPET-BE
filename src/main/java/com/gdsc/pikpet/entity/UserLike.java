60989950003cb404abd7888ba73f09c199ee30cf
yooonwodyd <dbswodyd00@naver.com>
2024-01-30 08:00:09 +0900

    #6 UserLike class 구현

---
+++ src/main/java/com/gdsc/pikpet/entity/UserLike.java
@@ -0,0 +1,18 @@
+package com.gdsc.pikpet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+public class UserLike {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Setter @ManyToOne @JoinColumn(name = "userId")
+    private UserAccount userAccount;
+
+    @Setter @ManyToOne @JoinColumn(name = "animalId")
+    private Animal animal;
+}

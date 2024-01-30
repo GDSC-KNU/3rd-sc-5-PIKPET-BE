9e039a027123be2ccb24d7ac1d827e59ccaabdb9
yooonwodyd <dbswodyd00@naver.com>
2024-01-31 02:17:37 +0900

    #9 userDetailService에서 사용할 findByEmail 추가

--- src/main/java/com/gdsc/pikpet/repository/UserAccountRepository.java
+++ src/main/java/com/gdsc/pikpet/repository/UserAccountRepository.java
@@ -4,6 +4,9 @@ import com.gdsc.pikpet.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
+    Optional<UserAccount> findByEmail(String email);
 }

ced4e78c2295222518bd51131d434583d1638801
yooonwodyd <dbswodyd00@naver.com>
2024-01-30 22:25:24 +0900

    #6 위도 경도를 Point type으로 변경

--- src/main/java/com/gdsc/pikpet/entity/Shelter.java
+++ src/main/java/com/gdsc/pikpet/entity/Shelter.java
@@ -3,6 +3,7 @@ package com.gdsc.pikpet.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.geo.Point;
 
 @Entity
 @Getter
@@ -11,10 +12,8 @@ public class Shelter {
     @Column(nullable = false, name = "shelter_id")
     private int id;
 
-    @Setter private double latitude;
-    @Setter private double longitude;
+    @Setter @Column(columnDefinition = "GEOMETRY") private Point location;
     @Setter private String branchName;
     @Setter private String contact;
     // Todo: mangerID의 경우 리팩토링이 필요할 수 있음. OneToOne으로 연결시 문제 가능성. ManyToOne으로 변경 고려
-    @Setter private int managerId;
 }

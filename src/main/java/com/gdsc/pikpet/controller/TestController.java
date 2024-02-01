e55fbb718769b0a2876fc2bc9fc2edbf02c5361f
jaehyeon <fanta4715@naver.com>
2024-02-01 10:14:01 +0900

    #12 feat: Storage객체 생성

--- src/main/java/com/gdsc/pikpet/controller/TestController.java
+++ src/main/java/com/gdsc/pikpet/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.gdsc.pikpet.controller;
 
+//import com.gdsc.pikpet.config.StorageConfig;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +14,12 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
-    // Google Cloud Storage 버킷 이름
-    @Value("${application.bucket.name}") 
-    private String bucketName;
+    private final Storage storage;
 
+    @Value("${gcs.bucket.name}")
+    private String bucketName;
     @GetMapping("/")
     public String test() {
         return "Hello World!";
@@ -22,23 +27,16 @@ public class TestController {
 
     @GetMapping("/testImage")
     public String testImage() throws IOException {
-        String imageName = "good.jpg"; // 업로드할 이미지 파일 이름
-
-        // 서비스 계정 키 파일 경로
-        String jsonPath = "";
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
-        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         // StorageOptions 생성 (인증 정보 설정 필요)
-        Blob blob = storage.get("solution-challenge-bucket", "spring.png");
-        blob.downloadTo(Paths.get("TestImage"));
-
+        Blob blob = storage.get(bucketName, "spring.png");
+        blob.downloadTo(Paths.get("TestImage.png"));
 
         // test시 resource 필드에 이미지+json 필요
         BlobInfo blobInfo =storage.create(
-                BlobInfo.newBuilder("solution-challenge-bucket", "springTest.png")
+                BlobInfo.newBuilder(bucketName, "springTest.png")
                         .setContentType("png")
                         .build(),
-                new FileInputStream("TestImage"));
+                new FileInputStream("TestImage.png"));
 
         // 업로드 성공 시 메시지 반환
         return "이미지 다운로드 완료: " + blob.getName();

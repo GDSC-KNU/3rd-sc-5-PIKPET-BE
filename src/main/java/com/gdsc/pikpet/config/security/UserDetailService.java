577f4d25a66a9a182c8e6d6efe49b13f817ca0d1
yooonwodyd <dbswodyd00@naver.com>
2024-02-04 01:47:35 +0900

    [hotfix] 라이브러리 추가

--- src/main/java/com/gdsc/pikpet/config/security/UserDetailService.java
+++ src/main/java/com/gdsc/pikpet/config/security/UserDetailService.java
@@ -1,6 +1,7 @@
 package com.gdsc.pikpet.config.security;
 
 import com.gdsc.pikpet.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;

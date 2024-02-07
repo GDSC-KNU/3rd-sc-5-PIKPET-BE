4fe3ad0aba8bbe99094a1963fe4dd68c851b4c67
yooonwodyd <dbswodyd00@naver.com>
2024-02-07 20:45:39 +0900

    #21 userAccountService에 관심동물 추가 서비스 구현. 추후 논의에 따라 별도의 서비스 클래스를 생성할 수도 있음.

--- src/main/java/com/gdsc/pikpet/service/UserAccountService.java
+++ src/main/java/com/gdsc/pikpet/service/UserAccountService.java
@@ -1,7 +1,11 @@
 package com.gdsc.pikpet.service;
 
 import com.gdsc.pikpet.config.security.UserSecurityDto;
+import com.gdsc.pikpet.entity.Animal;
 import com.gdsc.pikpet.entity.UserAccount;
+import com.gdsc.pikpet.entity.UserLike;
+import com.gdsc.pikpet.repository.AnimalRepository;
+import com.gdsc.pikpet.repository.LikeRepository;
 import com.gdsc.pikpet.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
@@ -12,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserAccountService {
     private final UserAccountRepository userAccountRepository;
+    private final AnimalRepository animalRepository;
+    private final LikeRepository likeRepository;
 
     @Transactional
     public UserSecurityDto updateUser(UserSecurityDto userSecurityDto) {
@@ -19,5 +25,12 @@ public class UserAccountService {
         userAccount.update(UserSecurityDto.toEntity(userSecurityDto));
         userAccountRepository.save(userAccount);
         return UserSecurityDto.from(userAccount);
+    }
+
+    @Transactional
+    public void addlikeAnimal(UserSecurityDto userSecurityDto, Long animalId) {
+        UserAccount userAccount = userAccountRepository.findByEmail(userSecurityDto.email()).orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다 - email: " + userSecurityDto.email()));
+        Animal animal = animalRepository.findById(animalId).orElseThrow(() -> new IllegalArgumentException("동물을 찾을 수 없습니다 - id: " + animalId));
+        likeRepository.save(UserLike.of(userAccount, animal));
     }
 }

e90c0497e41a551ba9f866d182f21dafce6db1eb
jaehyeon <fanta4715@naver.com>
2024-02-19 21:05:29 +0900

    #48 hotfix: CORS 허용

---
+++ src/main/java/com/gdsc/pikpet/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.gdsc.pikpet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //모든 경로에 대해
+        registry.addMapping("/**")
+                .allowedOrigins("/**")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}

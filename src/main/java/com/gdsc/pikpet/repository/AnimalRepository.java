a90cadf0aa71fd908f6cdbbf5f7a8727191c0a58
yooonwodyd <dbswodyd00@naver.com>
2024-02-19 16:21:07 +0900

    #41 위치 기반 검색을 위해 shelter entity 수정 및 repository 로직 작성

--- src/main/java/com/gdsc/pikpet/repository/AnimalRepository.java
+++ src/main/java/com/gdsc/pikpet/repository/AnimalRepository.java
@@ -1,9 +1,15 @@
 package com.gdsc.pikpet.repository;
 
 import com.gdsc.pikpet.entity.animal.Animal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    @Query("select animal from Animal animal order by (ST_Distance_Sphere(ST_MakePoint(animal.shelter.latitude,animal.shelter.location), ST_MakePoint(?1, ?2)))")
+    Page<Animal> findByLocation(double lat, double lon, Pageable pageable);
 }

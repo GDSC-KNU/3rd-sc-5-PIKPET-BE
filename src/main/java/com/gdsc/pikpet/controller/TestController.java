a4b5a02e3897694c7bb48cd63f08e49408365923
yooonwodyd <dbswodyd00@naver.com>
2024-02-19 19:15:50 +0900

    [hotfix] cors 적용

--- src/main/java/com/gdsc/pikpet/controller/TestController.java
+++ src/main/java/com/gdsc/pikpet/controller/TestController.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins="*")
 public class TestController {
 
     private final Storage storage;

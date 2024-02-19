4a23bcda77cfa74ebce24f1123ebcbea109981c8
yooonwodyd <dbswodyd00@naver.com>
2024-02-19 19:49:52 +0900

    url 위치 변경

--- src/main/java/com/gdsc/pikpet/config/security/SecurityConfig.java
+++ src/main/java/com/gdsc/pikpet/config/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http.formLogin(
                 formLogin ->
                         formLogin.loginPage("/loginPage/formLogin")
-                                .loginProcessingUrl("/login")
+                                .loginProcessingUrl("/loginPage/login")
                                 .usernameParameter("username")
                                 .passwordParameter("password")
                                 .defaultSuccessUrl("/loginPage/loginSuccess",true)// user 같이 정보 반환, 프런트 연결시 변경 필요

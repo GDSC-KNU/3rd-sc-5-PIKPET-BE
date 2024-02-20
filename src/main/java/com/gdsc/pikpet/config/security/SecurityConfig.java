c209bb3cb87cb1ce58caf9c44bbfbf8bbb115ec1
yooonwodyd <dbswodyd00@naver.com>
2024-02-20 12:22:18 +0900

    sercurity config 변경

--- src/main/java/com/gdsc/pikpet/config/security/SecurityConfig.java
+++ src/main/java/com/gdsc/pikpet/config/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                                         .successHandler(
                                                 (request, response, authentication) -> {
                                                     response.setStatus(HttpServletResponse.SC_OK);
-                                                    response.setHeader("Set-Cookie", "JSESSIONID=" + request.getSession().getId() + "; Path=/; HttpOnly");
+                                                    response.setHeader("Set-Cookie", "JSESSIONID=" + request.getSession().getId() + "; Path=/; HttpOnly; SameSite=None;");
                                                     response.getWriter().print("{\"success\": true, \"message\": \"Login successful.\", \"data\": {\"username\": \"" + authentication.getName() + "\"}}");
                                                     response.getWriter().flush();
                                                 }
